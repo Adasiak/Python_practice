c0fc4efcccdbcaf02195ed85401d80f107c1613c
Adasiak <258974@student.pwr.edu.pl>
2022-09-21 17:05:41 +0200

    xsx

--- Automation/MISC/mouse_move.py
+++ Automation/MISC/mouse_move.py
@@ -9,7 +9,7 @@ mouse_event = ctypes.windll.user32.mouse_event
 
 def left_click(x, y, clicks=1):
     SetCursorPos(x, y)
-    mouse_event(2, 0, 0, 0, 0)
+    mouse_event(2, 0, 0 , 0, 0)
     mouse_event(4, 0, 0, 0, 0)
 
 while True:
